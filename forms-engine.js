601c06115d659189870feefa8dd7bcd476525c70
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2017-12-01 15:56:10 +0100

    Update

--- forms-engine.js
+++ forms-engine.js
@@ -1,7 +1,23 @@
-function createForm(target, definition){
+function createForm(target, propertyDefinitions, formElements){
   var container = document.createElement('div');
 
-  
+  for(var i = 0; i < propertyDefinitions.length; i++){
+    var propertyDefinition = propertyDefinitions[i];
+
+    for(var j = 0; j < formElements.length; j++){
+      var formElement = formElements[j];
+
+      if(formElement.type != propertyDefinition.type){
+        continue;
+      }
+
+      container.appendChild(createFormElement(target, propertyDefinition, formElement));
+    }
+  }
 
   return container;
+}
+
+function createFormElement(target, propertyDefinition, formElement){
+  return formElement.create(function(){ return target[propertyDefinition.key]; }, function(value){ target[propertyDefinition.key] = value; });
 }
--- main.js
+++ main.js
@@ -7,9 +7,9 @@ var target = buildModel({
 
 
 
-var definitions = [
+var propertyDefinitions = [
   {
-    id: 'name',
+    key: 'name',
     name: 'Name',
     type: 'string'
   }
@@ -17,16 +17,34 @@ var definitions = [
 
 
 
-var elements = [
+var formElements = [
+  {
+    type: 'string',
+    create: function(get, set){
+      var element = document.createElement('input');
+      
+      element.type = 'text';
+      element.value = get();
 
+      function update(){
+        set(element.value);
+      }
+
+      element.addEventListener('change', update);
+      element.addEventListener('keyup', update);
+
+      return element;
+    }
+  }
 ];
 
 
 
-var form = createForm(target, definitions, elements);
+var form = createForm(target, propertyDefinitions, formElements);
 
 document.getElementById('my-form').appendChild(form);
 
 setInterval(function(){
   document.getElementById('debug').value = JSON.stringify(target);
+  document.getElementById('heading').innerText = target.name;
 }, 100);
